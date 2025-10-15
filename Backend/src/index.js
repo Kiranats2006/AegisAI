fa2efe56d50a8e92ca7e4b367ba2519dd6a36dd7
jeevanhd <jeevanhd1313@gmail.com>
2025-10-15 17:25:45 +0530

    Added models

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,15 @@
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "./config/env",
+    path: "./config/.env",
   });
 }
-// const connectDatabase = require("./DB/database.js");
+const connectDatabase = require("./DB/database.js");
 const app = require("./app.js");
 
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, async () => {
-//   connectDatabase();
+  connectDatabase();
   console.log(
     `The Server is running on Port:${PORT} URL: http://localhost:${PORT}`
   );
