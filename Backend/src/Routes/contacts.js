d0b3a2ecc9523dafa78c6c67bba20ce52317582b
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-10-15 17:23:44 +0530

    added emergency contacts routes and contollers

---
+++ Backend/src/Routes/contacts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../middleware/auth');
+const { addContact, getContacts, updateContact, deleteContact } = require('../controllers/contactController');
+
+const postContactRoute = router.post('/', addContact);
+const getContactsRoute = router.get('/', getContacts);
+const putContactRoute = router.put('/:id', updateContact);
+const deleteContactRoute = router.delete('/:id', deleteContact);
+
+module.exports = router;
